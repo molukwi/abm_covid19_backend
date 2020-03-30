6d9772c3ab3841b28ad89167419284722782ffe8
molukwi <molukwi@gmail.com>
2020-03-30 09:10:30 +0200

    Make that the content of the file is returned as string

--- src/main/java/server_tasks/Resource.java
+++ src/main/java/server_tasks/Resource.java
@@ -1,17 +1,15 @@
 package server_tasks;
 
-import org.apache.commons.io.IOUtils;
-import org.apache.commons.lang3.Validate;
+import com.google.common.io.Files;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Paths;
 
 @RestController
 @CrossOrigin(origins = "*")
@@ -30,34 +28,33 @@ public class Resource {
 
     private final ScriptPython scriptPython;
 
-    public Resource(ScriptPython scriptPython){
+    public Resource(ScriptPython scriptPython) {
         this.scriptPython = scriptPython;
     }
 
     @GetMapping(value = "data", produces = MediaType.APPLICATION_OCTET_STREAM_VALUE)
-    public byte[] getData(@RequestParam float random_encounters,
+    public String getData(@RequestParam float random_encounters,
                           @RequestParam float[] prob_communities,
                           @RequestParam float initial_fraction_infected,
                           @RequestParam float fraction_interacting,
                           @RequestParam float p_infection,
                           @RequestParam float p_contact) throws IOException {
-     final Parameters parameters =
-             new Parameters.Builder()
-                     .withRandomEncounters(random_encounters)
-                     .withProbCommunities(prob_communities)
-                     .withInitialFractionInfected(initial_fraction_infected)
-             .withFractionInteracting(fraction_interacting)
-             .withPInfection(p_infection)
-             .withPContact(p_contact).build();
+        final Parameters parameters =
+                new Parameters.Builder()
+                        .withRandomEncounters(random_encounters)
+                        .withProbCommunities(prob_communities)
+                        .withInitialFractionInfected(initial_fraction_infected)
+                        .withFractionInteracting(fraction_interacting)
+                        .withPInfection(p_infection)
+                        .withPContact(p_contact).build();
 
         scriptPython.runScript(pythonPath, scriptPath, parameters);
 
         return getJsonFile();
     }
 
-    private byte[] getJsonFile() throws IOException {
-        final InputStream is = new FileInputStream(jsonPath);
+    private String getJsonFile() throws IOException {
         LOG.info(jsonPath);
-        return IOUtils.toByteArray(is);
+        return Files.toString(Paths.get(jsonPath).toFile(), StandardCharsets.UTF_8);
     }
 }
